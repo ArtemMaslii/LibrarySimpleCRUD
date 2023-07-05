4f12065b45c830251290ea12035073b220245c53
Artem <maslijartei@gmail.com>
2023-07-05 16:06:43 +0300

    Add config classes, properties template

---
+++ src/main/java/com/learning/config/SpringConfig.java
@@ -0,0 +1,63 @@
+package com.learning.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan("com.learning")
+@EnableWebMvc
+@PropertySource("classpath:database.properties")
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        Properties properties = loadProperties();
+
+        dataSource.setDriverClassName(properties.getProperty("driver"));
+        dataSource.setUrl(properties.getProperty("url"));
+        dataSource.setUsername(properties.getProperty("username"));
+        dataSource.setPassword(properties.getProperty("password"));
+
+        return dataSource;
+    }
+
+    private Properties loadProperties() {
+        Properties properties = new Properties();
+            try(InputStream inputStream = getClass().getClassLoader().getResourceAsStream("database.properties")) {
+
+                properties.load(inputStream);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return properties;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
