7c55b847fa9e266017d911a12668b5aa922b7857
Artem <maslijartei@gmail.com>
2023-07-30 19:59:07 +0300

    BookDAO add release and assign methods. PersonDAO add get methods by id and name

--- src/main/java/com/learning/DAO/BookDAO.java
+++ src/main/java/com/learning/DAO/BookDAO.java
@@ -2,6 +2,7 @@ package com.learning.DAO;
 
 
 import com.learning.models.Book;
+import com.learning.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -28,18 +29,31 @@ public class BookDAO {
         return jdbcTemplate.query("SELECT * FROM Book WHERE bookId=?", new Object[]{bookId}, new BeanPropertyRowMapper<>(Book.class))
                 .stream().findAny().orElse(null);
     }
-
     public void save(Book newBook) {
-        jdbcTemplate.update("INSERT INTO Book(personId, name, author, yearOfPublish) VALUES (?, ?, ?, ?)",
-                newBook.getOwnerId(), newBook.getName(), newBook.getAuthor(), newBook.getYearOfPublish());
+        jdbcTemplate.update("INSERT INTO Book(name, author, yearOfPublish) VALUES (?, ?, ?)",
+                newBook.getName(), newBook.getAuthor(), newBook.getYearOfPublish());
     }
 
     public void update(int bookId, Book updatedBook) {
-        jdbcTemplate.update("UPDATE Book SET personId=?, name=?, author=?, yearOfPublish=? WHERE bookId=?",
-                updatedBook.getOwnerId(), updatedBook.getName(), updatedBook.getAuthor(), updatedBook.getYearOfPublish(), bookId);
+        jdbcTemplate.update("UPDATE Book SET name=?, author=?, yearOfPublish=? WHERE bookId=?",
+                updatedBook.getName(), updatedBook.getAuthor(), updatedBook.getYearOfPublish(), bookId);
     }
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Book WHERE bookId=?", id);
+    }
+
+    public Optional<Person> getBookOwner(int id) {
+        return jdbcTemplate.query("SELECT Person.* FROM Book JOIN Person ON Book.ownerId = Person.personId WHERE Book.bookId = ?",
+                new Object[] {id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
+    public void releaseBook(int id) {
+        jdbcTemplate.update("UPDATE Book SET ownerId=NULL WHERE bookId=?", id);
+    }
+
+    public void assignBook(int id, Person customer) {
+        jdbcTemplate.update("UPDATE Book SET ownerId=? WHERE id=?", customer.getPersonId(), id);
     }
 }
--- src/main/java/com/learning/DAO/PersonDAO.java
+++ src/main/java/com/learning/DAO/PersonDAO.java
@@ -1,12 +1,13 @@
 package com.learning.DAO;
 
+import com.learning.models.Book;
 import com.learning.models.Person;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class PersonDAO {
@@ -18,25 +19,30 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+        return this.jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper(Person.class));
     }
 
     public Person show(int personId) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE personId=?", new Object[]{personId}, new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny().orElse(null);
+        return (Person)this.jdbcTemplate.query("SELECT * FROM Person WHERE personId=?", new Object[]{personId}, new BeanPropertyRowMapper(Person.class)).stream().findAny().orElse((Object)null);
     }
 
     public void save(Person newPerson) {
-        jdbcTemplate.update("INSERT INTO Person(fullName, yearOfBirth) VALUES (?, ?)",
-                newPerson.getFullName(), newPerson.getYearOfBirth());
+        this.jdbcTemplate.update("INSERT INTO Person(fullName, yearOfBirth) VALUES (?, ?)", new Object[]{newPerson.getFullName(), newPerson.getYearOfBirth()});
     }
 
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET fullName=?, yearOfBirth=? WHERE id=?",
-                updatedPerson.getFullName(), updatedPerson.getYearOfBirth(), id);
+        this.jdbcTemplate.update("UPDATE Person SET fullName=?, yearOfBirth=? WHERE personId=?", new Object[]{updatedPerson.getFullName(), updatedPerson.getYearOfBirth(), id});
     }
 
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE personId=?", id);
+        this.jdbcTemplate.update("DELETE FROM Person WHERE personId=?", new Object[]{id});
+    }
+
+    public Optional<Person> getPersonByFullName(String fullName) {
+        return this.jdbcTemplate.query("SELECT * FROM Person WHERE fullName=?", new Object[]{fullName}, new BeanPropertyRowMapper(Person.class)).stream().findAny();
+    }
+
+    public List<Book> getBooksByPersonId(int personId) {
+        return this.jdbcTemplate.query("SELECT * FROM Book WHERE ownerId=?", new Object[]{personId}, new BeanPropertyRowMapper(Book.class));
     }
 }
