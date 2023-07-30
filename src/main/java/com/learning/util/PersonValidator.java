7b730ddedda09106715e98a27982725f146bc6e8
Artem <maslijartei@gmail.com>
2023-07-30 19:57:51 +0300

    Validator updated to switch to Thymeleaf for unique names

--- src/main/java/com/learning/util/PersonValidator.java
+++ src/main/java/com/learning/util/PersonValidator.java
@@ -2,28 +2,27 @@ package com.learning.util;
 
 import com.learning.DAO.PersonDAO;
 import com.learning.models.Person;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Component
 public class PersonValidator implements Validator {
-
     private final PersonDAO personDAO;
 
     public PersonValidator(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
-    @Override
     public boolean supports(Class<?> clazz) {
         return Person.class.equals(clazz);
     }
 
-    @Override
     public void validate(Object target, Errors errors) {
-        Person person = (Person) target;
-
-        if (personDAO.getPersonByFullName(person.getFullName()).isPresent()) {
+        Person person = (Person)target;
+        if (this.personDAO.getPersonByFullName(person.getFullName()).isPresent()) {
             errors.rejectValue("fullName", "", "Such person already exists");
         }
+
     }
 }
