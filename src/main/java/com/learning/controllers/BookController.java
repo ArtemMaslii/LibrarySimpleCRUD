fbc0055bc5e0f07c103f5c824a5372976e1a6697
Artem <maslijartei@gmail.com>
2023-07-30 20:01:15 +0300

    BookController add release and assign; modified some annotations to Patch instead of Post. PersonController modified some annotations to Patch instead of Post; add validator to check unique of names

--- src/main/java/com/learning/controllers/BookController.java
+++ src/main/java/com/learning/controllers/BookController.java
@@ -1,34 +1,106 @@
 package com.learning.controllers;
 
 import com.learning.DAO.BookDAO;
+import com.learning.DAO.PersonDAO;
+import com.learning.models.Book;
+import com.learning.models.Person;
+import jakarta.validation.Valid;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping({"/books"})
 public class BookController {
-
     private final BookDAO bookDAO;
+    private final PersonDAO personDAO;
 
     @Autowired
-    public BookController(BookDAO bookDAO) {
+    public BookController(BookDAO bookDAO, PersonDAO personDAO) {
         this.bookDAO = bookDAO;
+        this.personDAO = personDAO;
     }
 
-    @GetMapping()
+    @GetMapping
     public String index(Model model) {
-        model.addAttribute("bookList", bookDAO.index());
+        model.addAttribute("bookList", this.bookDAO.index());
         return "/books/index";
     }
 
-    @GetMapping("/{bookId}")
+    @GetMapping({"/{bookId}"})
     public String show(@PathVariable("bookId") int bookId, Model model) {
-        model.addAttribute("book", bookDAO.show(bookId));
-        return "/books/show";
+        model.addAttribute("book", this.bookDAO.show(bookId));
+        Optional<Person> owner = this.bookDAO.getBookOwner(bookId);
+        if (owner.isPresent()) {
+            model.addAttribute("owner", owner.get());
+        } else {
+            model.addAttribute("people", this.personDAO.index());
+        }
+
+        return "books/show";
     }
 
+    @GetMapping({"/new"})
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("book") @Valid Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "books/new";
+        } else {
+            this.bookDAO.save(book);
+            return "redirect:/books";
+        }
+    }
+
+    @GetMapping({"/{bookId}/edit"})
+    public String edit(@PathVariable("bookId") int bookId, Model model) {
+        model.addAttribute("book", this.bookDAO.show(bookId));
+        return "books/edit";
+    }
+
+    @PatchMapping({"/{bookId}"})
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult result, @PathVariable("bookId") int bookId) {
+        if (result.hasErrors()) {
+            return "books/edit";
+        } else {
+            this.bookDAO.update(bookId, book);
+            return "redirect:/books";
+        }
+    }
+
+    @DeleteMapping({"/{bookId}"})
+    public String delete(@PathVariable("bookId") int bookId) {
+        this.bookDAO.delete(bookId);
+        return "redirect:/books";
+    }
+
+    @PatchMapping({"/{bookId}/release"})
+    public String release(@PathVariable("bookId") int bookId) {
+        this.bookDAO.releaseBook(bookId);
+        return "redirect:/books/" + bookId;
+    }
+
+    @PostMapping({"/{bookId}/assign"})
+    public String assign(@PathVariable("bookId") int bookId, @RequestParam int personId) {
+        Person customer = this.personDAO.show(personId);
+        if (customer == null) {
+            return "redirect:/books";
+        } else {
+            this.bookDAO.assignBook(bookId, customer);
+            return "redirect:/books/" + bookId;
+        }
+    }
 }
--- src/main/java/com/learning/controllers/PersonController.java
+++ src/main/java/com/learning/controllers/PersonController.java
@@ -1,34 +1,80 @@
 package com.learning.controllers;
 
-
 import com.learning.DAO.PersonDAO;
+import com.learning.models.Person;
+import com.learning.util.PersonValidator;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/people")
+@RequestMapping({"/people"})
 public class PersonController {
-
     private final PersonDAO personDAO;
+    private final PersonValidator validator;
 
     @Autowired
-    public PersonController(PersonDAO personDAO) {
+    public PersonController(PersonDAO personDAO, PersonValidator validator) {
         this.personDAO = personDAO;
+        this.validator = validator;
     }
 
-    @GetMapping()
+    @GetMapping
     public String index(Model model) {
-        model.addAttribute("personList", personDAO.index());
+        model.addAttribute("personList", this.personDAO.index());
         return "people/index";
     }
 
-    @GetMapping("/{personId}")
+    @GetMapping({"/{personId}"})
     public String show(@PathVariable("personId") int personId, Model model) {
-        model.addAttribute("person", personDAO.show(personId));
+        model.addAttribute("person", this.personDAO.show(personId));
+        model.addAttribute("books", this.personDAO.getBooksByPersonId(personId));
         return "/people/show";
+    }
+
+    @GetMapping({"/new"})
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/people/new";
+        } else {
+            this.personDAO.save(person);
+            return "redirect:/people";
+        }
+    }
+
+    @GetMapping({"/{personId}/edit"})
+    public String edit(@PathVariable("personId") int personId, Model model) {
+        model.addAttribute("person", this.personDAO.show(personId));
+        return "people/edit";
+    }
+
+    @PatchMapping({"/{personId}"})
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult result, @PathVariable("personId") int personId) {
+        if (result.hasErrors()) {
+            return "people/edit";
+        } else {
+            this.personDAO.update(personId, person);
+            return "redirect:/people";
+        }
+    }
+
+    @DeleteMapping({"/{personId}"})
+    public String delete(@PathVariable("personId") int personId) {
+        this.personDAO.delete(personId);
+        return "redirect:/people";
     }
 }
